b359204e40817254aeb669aae1077231858f5b15
ryseymour <ry.seymour@gmail.com>
2020-08-20 18:27:17 -0400

    Reset overworld scene

---
+++ TopDownRPG/Assets/Scripts/Grid/GridTesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridTesting : MonoBehaviour
+{
+    private GridClass grid;
+
+    private void Start()
+    {
+         grid = new GridClass(4, 2, 10f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 22);
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+
+        }
+    }
+}
