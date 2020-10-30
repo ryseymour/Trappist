147389e37038681f4695f5a522e8146704fffab7
colewrus <wrampelc@gmail.com>
2020-10-29 20:02:08 -0400

    start to add vfx to battle scene

---
+++ TopDownRPG/Assets/Scenes/unit testing/battleAnims.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class battleAnims : MonoBehaviour
+{
+
+    public Battler myBattler;
+    public List <Battler> party = new List<Battler>();
+    public List <GameObject> partyObj = new List<GameObject>();
+
+    public List <Battler> enemies = new List<Battler>();
+    public List <GameObject> enemyObj = new List<GameObject>();
+    // Start is called before the first frame update
+
+    public GameObject floatText;
+
+    public int partySelect, enemySelect;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad1)){
+          
+            string dmg = "" + party[partySelect].myAbilities[0].Damage.ToString();            
+
+            AddText(dmg);
+            // print(party[partySelect].myAbilities[0].myName);
+
+            // partyObj[partySelect].transform.GetChild(0).gameObject.SetActive(true);
+            // partyObj[partySelect].transform.GetChild(0).GetComponent<TextMesh>().text = party[partySelect].myAbilities[0].myName;
+        }
+    }
+
+
+    void AddText(string txt){
+        floatText.GetComponent<TextMesh>().text = "" + txt;
+        Instantiate(floatText, partyObj[partySelect].transform.position, Quaternion.identity);
+        
+    }
+}
---
+++ TopDownRPG/Assets/Scenes/unit testing/textAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textAnimation : MonoBehaviour
+{
+
+
+    public Vector3 Offset = new Vector3(0, 11, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake(){        
+        Destroy(gameObject, 3.0f);
+
+        transform.localPosition += Offset;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator activeDelay(float t){
+        yield return new WaitForSeconds(t);
+        print(gameObject);
+    }
+}
