fd6b55d12ff257f3a5c19cb8cba2b0cc4dd587b1
colewrus <wrampelc@gmail.com>
2020-12-06 18:34:03 -0500

    town testing

---
+++ TopDownRPG/Assets/Scenes/unit testing/Town/TownBuilding.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownBuilding : MonoBehaviour
+{
+
+
+    public Town myAssociatedTown;
+    public Building myAssociatedBuilding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tParent = GameObject.Find("TownParent");
+        if(tParent != null){
+            myAssociatedTown = tParent.GetComponent<TownParent>().activeTown;
+        }
+    }
+
+
+
+    void OnMouseDown(){
+        Debug.Log("clicked me");    
+    }
+}
