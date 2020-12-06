30fc8628396eeb17b510814a27122f9179058075
colewrus <wrampelc@gmail.com>
2020-12-06 18:55:32 -0500

    town changes, mostly trying to merge

---
+++ TopDownRPG/Assets/Scenes/unit testing/Town/TownBuilding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownBuilding : MonoBehaviour
+{
+
+
+    public Town myAssociatedTown;
+    public Building myAssociatedBuilding;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+
+
+    void OnMouseDown(){
+        Debug.Log("clicked me");    
+        GameObject tParent = GameObject.Find("TownParent");
+        if(tParent != null){
+           
+        }
+    }
+}
