5dcbd3b40d36e69183f446f919debc1b94ac8292
ryseymour <ry.seymour@gmail.com>
2020-12-20 14:40:51 -0500

    People and Dialogue are loading with buidlings. Quests are a bit broken.

--- TopDownRPG/Assets/Scripts/Town/BuildingInteraction.cs
+++ TopDownRPG/Assets/Scripts/Town/BuildingInteraction.cs
@@ -48,6 +48,9 @@ public class BuildingInteraction : MonoBehaviour
          "\nCharacter 2: " + _building.inBuildingCharactersScene1[1] +
          "\nCharacter 3: " + _building.inBuildingCharactersScene1[2] +
          "\nCharacter 4: " + _building.inBuildingCharactersScene1[3]);
+        InsideManager.instance.buildingInside = _building;
+        InsideManager.instance.EnterBuilding(0);
+
     }
 
 }
